0e22a10c1fb662097edbcb7510ad9cf5dd427fd8
gopikiran-glitch <gopikiranjavafullstacktraine@gmail.com>
2025-11-13 14:38:11 +0530

    Delete Leetcode-main/Leetcode-main directory

--- Leetcode-main/Leetcode-main/arrays/1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def find_products(products: pd.DataFrame) -> pd.DataFrame:
-    return products.loc[(products['low_fats']=='Y')& (products['recyclable']=='Y'),['product_id']]
-    
--- Leetcode-main/Leetcode-main/pandas/problems/2883-drop-missing-data/2883-drop-missing-data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    students=students.dropna(subset='name')
-    return students
-    
